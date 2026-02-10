f1b9bdb532d9043bfc49aff98cb01618951aa377
EfratGrinboom <92879754+EfratGrinboom@users.noreply.github.com>
2026-02-10 23:54:29 +0200

    fix app code changes

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -1,29 +1,13 @@
-package com.mycompany.app;
+package com.myapp;
 
-import com.sun.net.httpserver.HttpServer;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpExchange;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-
-public class App {
-    public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        
-        server.createContext("/", new HttpHandler() {
-            @Override
-            public void handle(HttpExchange exchange) throws IOException {
-                String response = "Hello World from Efrat Hager! The server is UP.";
-                exchange.sendResponseHeaders(200, response.length());
-                OutputStream os = exchange.getResponseBody();
-                os.write(response.getBytes());
-                os.close();
-            }
-        });
-
-        System.out.println("Starting server on port 8080...");
-        server.setExecutor(null); 
-        server.start();
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from Efrat Hager!" );
     }
 }
